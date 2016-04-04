f807f2e74f84a9b06b14fb086851042a5f9e9cf7
Gaston Caceres <gaston.caceres@globant.com>
2016-04-04 15:56:51 -0300

    se arreglo pom.xml

--- Hello1/src/test/java/HelloTest.java
+++ Hello1/src/test/java/HelloTest.java
@@ -9,10 +9,10 @@ import org.junit.Test;
 import junit.framework.TestCase;
 import src.main.java.com.gcaceres.Hello;
 
-public class HelloTest extends TestCase{
+public class HelloTest extends TestCase {
 
 	private Hello hello;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -23,18 +23,18 @@ public class HelloTest extends TestCase{
 
 	@Before
 	public void setUp() throws Exception {
-		//instanciacion de objetos, apertura de archivos
+		// instanciacion de objetos, apertura de archivos
 		this.hello = new Hello();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//limpieza de memoria, cerrar archivos, etc
+		// limpieza de memoria, cerrar archivos, etc
 	}
 
 	@Test
 	public void testConnectToAndQueryDatabase() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 }
