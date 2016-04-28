e78df4594478b36c2beddfb399bdb234dce705eb
Gaston Caceres <gaston.caceres@globant.com>
2016-04-28 17:11:58 -0300

    working

--- gaston_caceres_mod0_ej2/src/main/java/gaston_caceres/training/globant/com/search/FlightSearch.java
+++ gaston_caceres_mod0_ej2/src/main/java/gaston_caceres/training/globant/com/search/FlightSearch.java
@@ -15,9 +15,8 @@ public class FlightSearch {
 
 	private static final String FLIGHTS_CONTAINER_ID = "flightModuleList";
 
-	private WebElement flightSorter;
 	private Select flightSort;
-	
+
 	private List<WebElement> flightsFound;
 	private WebDriver webDriver;
 
@@ -30,33 +29,35 @@ public class FlightSearch {
 	}
 
 	private void initFields() {
-//		this.flightSorter = (new WebDriverWait(webDriver, 10))
-//				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='sortBar']//select")));
 		this.flightSort = new Select((new WebDriverWait(webDriver, 10))
 				.until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='sortBar']//select"))));
-		loadFlightsFound();
 	}
 
 	private void loadFlightsFound() {
-		WebElement container = (new WebDriverWait(webDriver, 10))
-				.until(ExpectedConditions.presenceOfElementLocated(By.id(FLIGHTS_CONTAINER_ID)));
-		flightsFound = container.findElements(By.xpath("div/li"));
+		WebElement container = null;
+		try {
+			container = (new WebDriverWait(webDriver, 3))
+					.until(ExpectedConditions.presenceOfElementLocated(By.id("super_dummy")));
+		} catch (Exception e) {
+		} finally {
+			container = webDriver.findElement(By.id(FLIGHTS_CONTAINER_ID));
+			flightsFound = container.findElements(By.xpath("div/li"));
+
+		}
 	}
 
 	public FlightSearch sortFlights(FlightSort sortBy) {
 		this.flightSort.selectByVisibleText(sortBy.text());
-		loadFlightsFound();
 		return this;
 	}
 
 	public FlightSearch selectFlight(int flightPosition) {
 		try {
-//			this.flightSelected = (new WebDriverWait(webDriver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='"+FLIGHTS_CONTAINER_ID+"']/div/li[" + flightPosition + "]")));
+			loadFlightsFound();
 			this.flightSelected = this.flightsFound.get(flightPosition - 1);
 			String xpath = getFlightBuyXpath();
-			WebElement buy = (new WebDriverWait(webDriver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
-//			WebElement buy = flightSelected.findElement(By.xpath("//button"));
-//			this.flightSelected.findElement(By.xpath("//button")).click();
+			WebElement buy = (new WebDriverWait(webDriver, 10))
+					.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
 			buy.click();
 		} catch (IndexOutOfBoundsException e) {
 			e.printStackTrace();
